3cf144e494d1aa1e15dd479611dc8bc8964cf0ed
omerzk <mine!>
2016-08-15 12:06:47 +0300

    multer

--- app.js
+++ app.js
@@ -4,16 +4,41 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var app = express();
 var uuid = require('node-uuid');
 var child = require("child_process");
 var mkdir = require("mkdirp");
+var multer  =   require('multer');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//configure multer file upload
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    var id =  uuid.v1();
+    var dirPath = "output/" + id + "/";
+
+    //keep track of the output made/sent to the client.
+    openReq[id] =  {next:1, maxAvailable: 0, pendingRes:res};
+    res.body.id = id;
+
+    mkdir(dirPath, null, ()=>{
+      callback(null, dirPath);
+    });
+
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname);
+  }
+});
+
+var upload = multer({ storage : storage }).array('sourceImages',2);
+
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 
 //constants
@@ -24,52 +49,66 @@ var output = "-output_image output.png";
 
 var openReq = {};
 
-app.post('/api/process/content', (req, res, nxt)=>{
-  var id =  uuid.v1();
-  var dirPath = "output/" + id + "/";
-  mkdir(dirPath, null, ()=>{
-    var contentPath = dirPath + "_content";
-    fs.writeFileSync(contentPath, req.body);
-  });
-  res.statusCode(200).send("Style Still Needed");
-});
 
-app.post('/api/process/style', (req, res, nxt)=>{
-  var id =  uuid.v1();
-  var dirPath = "output/" + id + "/";
-  var stylePath = dirPath + "_style";
-  fs.writeFileSync(stylePath, req.body);
-  //run the neural net torch implementation
-  var spawn = child.spawn;
-  var process = spawn('th',["~/", '../neural_style.lua', numIter, "-style_image " + stylePath,
-    "-content_image" + contentPath, imageSz, backEnd, output]);
+app.post('/api/process', (req, res, nxt)=>{
+  upload(req,res,function(err) {
+    if(err) {
+      return res.end("Error uploading files.");
+    }
+    var id = res.body.id;
+    console.log("post, id: " + id);
+    //run the neural net torch implementation
+    var spawn = child.spawn;
+    var process = spawn('th',["~/", '../neural_style.lua', numIter, "-style_image " + stylePath,
+      "-content_image" + contentPath, imageSz, backEnd, output]);
+    //ack and send identifier
+    res.statusCode(200).send();
 
-  //keep track of the output made/sent to the client.
-  openReq[id] =  {next:1, maxAvailable: 0, pendingRes:res};
-  res.body.id = id;
-  res.statusCode(200).send();
-
-  process.stdout.on('data', ()=>{
-    openReq[id].maxAvailable++;
-    outputFrame(id);
+    process.stdout.on('data', ()=>{
+      openReq[id].maxAvailable++;
+      outputFrame(id);
     })
+
+  })
 });
 
 app.post('/api/getframe', (req, res)=>{
   var id = req.body.id;
   if(id != null && openReq[id] != null){
+    //store the res, respond when we have results.
     openReq[id].pendingRes = res;
     outputFrame(id);
   }
 });
 
+function logErr(funcName, err) {
+  if(err)
+    console.log(funcName + " error: " + err);
+}
 function outputFrame(id){
   var reqStatus = openReq[id];
   var dirPath = "output/" + id + "/";
+
   var next = (reqStatus.next * 100) % 1000;
   next = next ? "_" + next.toString() : "";
+
   if(reqStatus.maxAvailable >= reqStatus.next && reqStatus.pendingRes != null){
-    reqStatus.pendingRes.sendFile(dirPath + "output" + next + ".png");
+    var p = path.join(__dirname, dirPath + "output" + next + ".png" );
+    reqStatus.pendingRes.sendFile(p, {}, (err)=>{
+      if(err){
+        return logErr('SendFile', err);
+      }
+
+      reqStatus.next++;
+      //remove file that was already sent
+      if(next > 0){
+        fs.unlink(p, (err) => logErr('unlink', err));
+      }
+      else{
+        //end of operation remove entire folder and source files
+        rimraf(dirPath, (err) => logErr('unlink', err));
+      }
+    });
   }
 }
 
